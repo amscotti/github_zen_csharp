fa5c4afa88d660d163dab178c5c44448327d2643
Anthony Scotti <amscotti@users.noreply.github.com>
2020-06-07 20:47:30 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace github_zen_csharp
+{
+    class Program
+    {
+        const string apiURL = "https://api.github.com/zen";
+        const string userAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)";
+        public static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+
+
+            client.Headers.Add("user-agent", userAgent);
+
+            Stream data = client.OpenRead(apiURL);
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+            Console.WriteLine(s);
+            data.Close();
+            reader.Close();
+        }
+    }
+}
